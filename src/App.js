ba3d2fab62b084ba8dd160489340833bdb1f9215
vistritpandey <vistritpandey@gmail.com>
2020-11-17 16:13:29 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
       <p>Twitter OP</p>
+      <Sidebar />
+
+      {/* Feed */}
+
+      {/* Widget */}
     </div>
   );
 }
