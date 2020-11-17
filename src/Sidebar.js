0eaea0e1b3d076cddd2d606b23de5c5348a1b5b1
vistritpandey <vistritpandey@gmail.com>
2020-11-17 16:37:32 +0530

    m

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import './Sidebar.css'
+import SidebarOption from './SidebarOption'
 import TwitterIcon from '@material-ui/icons/Twitter'
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <TwitterIcon />
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
-            {/* SidebarOption */}
+            <SidebarOption />
             
             {/* Tweet button */}
             
