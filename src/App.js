1c135a0e2a71a8ce2f4fff9e7e2736ebac3aa927
vistritpandey <vistritpandey@gmail.com>
2020-11-18 19:02:06 +0530

    state provider added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import Sidebar from './Sidebar';
 import Feed from "./Feed";
 import Widgets from "./Widgets";
+import Login from "./Login";
 
 function App() {
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     <div className="app">
       <Sidebar />
