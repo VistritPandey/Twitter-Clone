d4e0f1bc8de21dfc3f3005c739b1fba2b3c92589
vistritpandey <vistritpandey@gmail.com>
2020-11-18 18:07:48 +0530

    TweetBox styled

--- src/TweetBox.js
+++ src/TweetBox.js
@@ -11,8 +11,8 @@ function TweetBox() {
     e.preventDefault();
 
     db.collection("posts").add({
-      displayName: "Rafeh Qazi",
-      username: "cleverqazi",
+      displayName: "Vistrit",
+      username: "VistritPandey",
       verified: true,
       text: tweetMessage,
       image: tweetImage,
