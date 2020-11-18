e3daf0835a280de2341a84779d139ab38e6583c4
vistritpandey <vistritpandey@gmail.com>
2020-11-18 18:19:06 +0530

    widget

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import "./Widgets.css";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Widgets() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <SearchIcon className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+
+      <div className="widgets__widgetContainer">
+        <h2>What's happening</h2>
+
+        <TwitterTweetEmbed tweetId={""} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="Vistrit"
+          options={{ height: 400 }}
+        />
+
+        <TwitterShareButton
+          url={"https://vistritpandey.me"}
+          options={{ text: "#I am the biggest noob", via: "okay dokie" }}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Widgets
+export default Widgets;
