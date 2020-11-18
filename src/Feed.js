74b496d50d437317e18b3741254d722f2cb14071
vistritpandey <vistritpandey@gmail.com>
2020-11-18 18:12:19 +0530

    tweetbox added

--- src/Feed.js
+++ src/Feed.js
@@ -2,11 +2,19 @@ import React from 'react'
 import "./Feed.css";
 import db from "./firebase";
 import FlipMove from "react-flip-move";
+import TweetBox from "./TweetBox";
 
 function Feed() {
     return (
-        <div>
+        <div className="feed">
+            <div className="feed__header">
+                <h2>Home</h2>
+            </div>
+
+            <TweetBox />
             
+            <FlipMove>
+            </FlipMove>
         </div>
     )
 }
