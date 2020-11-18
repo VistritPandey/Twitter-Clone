37d7707e182dfdbcda2cb57f571d70a8d8fc8779
vistritpandey <vistritpandey@gmail.com>
2020-11-18 17:42:36 +0530

    App updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Sidebar from './Sidebar';
+import Feed from "./Feed";
+import Widgets from "./Widgets";
 
 function App() {
   return (
     <div className="app">
       <Sidebar />
-
-      {/* Feed */}
-
-      {/* Widget */}
+      <Feed />
+      <Widgets />
     </div>
   );
 }
